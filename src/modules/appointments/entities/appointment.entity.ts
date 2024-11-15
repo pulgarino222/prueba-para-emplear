628958aa85a6d7f2af9a4047afc4c493bf86a23e
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-11-15 17:24:10 -0500

    feat:i created all entities and the relation them

--- src/modules/appointments/entities/appointment.entity.ts
+++ src/modules/appointments/entities/appointment.entity.ts
@@ -1,1 +1,40 @@
-export class Appointment {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne } from "typeorm";
+import { Patient } from "../../patients/entities/patient.entity";
+import { User } from "../../users/entities/user.entity";
+
+@Entity()
+export class Appointment {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @CreateDateColumn()
+    updatedAt: Date;
+
+    @Column()
+    date: Date;
+
+    @Column()
+    time: string;
+
+    @Column()
+    description: string;
+
+    @Column({ default: 'pending' })
+    status: string; 
+
+    @Column()
+    specialty: string; 
+
+    @Column()
+    reason: string; 
+
+    @ManyToOne(() => User, user => user.appointments)
+    doctor: User; 
+
+    @ManyToOne(() => Patient, patient => patient.appointments)
+    patient: Patient; 
+}
+
--- src/modules/patients/dto/create-patient.dto.ts
+++ src/modules/patients/dto/create-patient.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePatientDto {}
+import { IsString, IsDateString, IsPhoneNumber } from 'class-validator';
+
+export class CreatePatientDto {
+  @IsString()
+  fullName: string;
+
+  @IsDateString()
+  dateOfBirth: Date;
+
+  @IsString()
+  gender: string;
+
+  @IsPhoneNumber()
+  contactNumber: string;
+
+  @IsString()
+  address: string;
+}
